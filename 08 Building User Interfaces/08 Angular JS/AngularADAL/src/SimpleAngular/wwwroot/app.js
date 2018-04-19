0d933216a0b9c4393bb2ca194acd2793c4371988
Alexander Pajer <alexander.pajer@integrations.at>
2018-04-19 09:53:42 +0200

    Angular ADAL

--- 08 Building User Interfaces/08 Angular JS/AngularADAL/src/SimpleAngular/wwwroot/app.js
+++ 08 Building User Interfaces/08 Angular JS/AngularADAL/src/SimpleAngular/wwwroot/app.js
@@ -1,19 +1,15 @@
 ﻿(function () {
     
-    var voucherApp = angular.module('voucherApp', ['ngRoute', 'AdalAngular']);
+    var eventsApp = angular.module('eventsApp', ['ngRoute', 'AdalAngular']);
     
-    voucherApp.config(["$routeProvider", "$httpProvider", "adalAuthenticationServiceProvider",
+    eventsApp.config(["$routeProvider", "$httpProvider", "adalAuthenticationServiceProvider",
     function ($routeProvider, $httpProvider, adalProvider) {
 
         var adalConfig = {
             tenant: "d92b247e-90e0-4469-a129-6a32866c0d0a",
             clientId: "4e60c128-a813-4031-bd99-cf4327cce885", //=> Application ID in Azure
             cacheLocation: "localStorage",
-            endpoints: {
-                'https://integrationsonline.sharepoint.com/_api/': 'https://integrationsonline.sharepoint.com',
-                'https://integrationsonline-my.sharepoint.com/_api/v1.0/me': 'https://integrationsonline-my.sharepoint.com',
-                'https://graph.microsoft.com': 'https://graph.microsoft.com'
-            }
+            endpoints: { 'https://graph.microsoft.com': 'https://graph.microsoft.com' }
         }
         adalProvider.init(adalConfig, $httpProvider);
 
@@ -30,14 +26,17 @@
         }
     ]);
 
-    voucherApp.controller('homeController', function ($scope) {
+    eventsApp.controller('homeController', function ($scope) {
+        //MVC - Model - View - Controller
+        //MVVM - Model - View - View Model
+        //$scope -> View Model
         $scope.startText = 'Welcome to the ADAL-Angular Web!';
     });
 
-    voucherApp.controller('eventsController', function ($scope, $http, adalAuthenticationService) {
+    eventsApp.controller('eventsController', function ($scope, $http, adalAuthenticationService) {
 
         if (adalAuthenticationService.userInfo.isAuthenticated) {
-            $scope.message = "Welcome to Events. You are logged in as " + adalAuthenticationService.userInfo.userName;
+            $scope.message = "Welcome to Events.<br>You are logged in as " + adalAuthenticationService.userInfo.userName;
         }
         else {
             $location.path("/");

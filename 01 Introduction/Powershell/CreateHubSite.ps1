a65d8042479a1bd14413fe4160f8332534c7c494
Administrator@SPDOM.LOCAL <Administrator@SPDOM.LOCAL>
2018-05-14 13:42:12 +0200

    ps update

--- 01 Introduction/Powershell/CreateHubSite.ps1
+++ 01 Introduction/Powershell/CreateHubSite.ps1
@@ -16,12 +16,10 @@ Connect-SPOService -Url $adminUrl
 
 Register-SPOHubSite -Site $hubSiteUrl
 
-Set-SPOHubSite -Identity $hubSiteUrl -LogoUrl $hubSiteUrl"/SiteAssets/learning.png" -Description "Main site for learning"
-
 Grant-SPOHubSiteRights -Identity $hubSiteUrl -Principals $user -Rights Join
 
 # Add site to be integrated
 
 New-PnPSite -Type TeamSite -title "Sample TS" -alias "samplets" -Description "A site to be integrated in the hub"
 
-Add-SPOHubSiteAssociation -Site https://$tenant.sharepoint.com/sites/samplets -HubSite https://$tenant.sharepoint.com/sites/$hubUrlFragment 
+Add-SPOHubSiteAssociation -Site https://$tenant.sharepoint.com/sites/samplets -HubSite $hubSiteUrl 

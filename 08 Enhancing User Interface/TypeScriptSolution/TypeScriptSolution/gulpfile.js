ce937c837f57fee4b4119b94a53cd8fd1c16b576
Alexander Pajer <alexander.pajer@integrations.at>
2018-05-15 20:24:42 +0200

    update

--- 08 Enhancing User Interface/TypeScriptSolution/TypeScriptSolution/gulpfile.js
+++ 08 Enhancing User Interface/TypeScriptSolution/TypeScriptSolution/gulpfile.js
@@ -1,20 +1,24 @@
-﻿var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+﻿var gulp = require("gulp");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var slash = require("slash");
 
-var paths = {    
-    scss: "./src/sass/**/*.scss",
-    scssDest: "./Layouts/TypeScriptSolution/dist"
-}
+var paths = {
+  scss: "./src/sass/**/*.scss",
+  dist: "./Layouts/TypeScriptSolution/dist",
+  sources: "./Layouts/TypeScriptSolution/dist/**.*.*",
+  prod: "D:\\test"
+};
 
-gulp.task('compile:sass', function () {
-    gulp.src(paths.scss)
-        .pipe(sass())
-        .pipe(gulp.dest(paths.scssDest));
+gulp.task("compile:sass", function() {
+  gulp
+    .src(paths.scss)
+    .pipe(sass())
+    .pipe(gulp.dest(paths.dist));
 });
 
-//watchers
+//watch sass
 
-gulp.task('watch:sass', function () {
-    gulp.watch(paths.scss, ['compile:sass']);
+gulp.task("watch:sass", function() {
+  gulp.watch(paths.scss, ["compile:sass"]);
 });

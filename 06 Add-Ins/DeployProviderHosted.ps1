e7a05d615d63fb3f80966904644cb33b0f8f79f8
Alexander Pajer <alexander.pajer@integrations.at>
2018-05-15 21:17:39 +0200

    provider hosted checked

--- 06 Add-Ins/DeployProviderHosted.ps1
+++ 06 Add-Ins/DeployProviderHosted.ps1
@@ -1,19 +1,18 @@
-if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) 
-{
+if ((Get-PSSnapin "Microsoft.SharePoint.PowerShell" -ErrorAction SilentlyContinue) -eq $null) {
     Add-PSSnapin "Microsoft.SharePoint.PowerShell"
 }
 
 # Make sure you have created the SecurityTokenIssuer using CreateSecurityTokenIssuer.ps1
-# Run only one or delete bevore creating
+# Run only once or delete bevore creating
 
 $site = "http://sp2016"
 $appName = "ProviderHostedAddIn"
-$issuerId = "2ffbee9b-3a1f-4d7e-b673-866ffa549d27"
+$clientId = "bdc2fb21-acc2-418b-b061-2430afc4207e"
 $appFile = "D:\Classes\O365ClientSideDev\06 Add-Ins\ProviderHostedAddIn\ProviderHostedAddIn\bin\Debug\app.publish\1.0.0.0\ProviderHostedAddIn.app"
 
 $web = Get-SPWeb -Identity $site
 $realm = Get-SPAuthenticationRealm -ServiceContext $web.Site;
-$appIdentifier = $issuerId  + '@' + $realm;
+$appIdentifier = $clientId + '@' + $realm;
 
 # Register the App with given IssuerId /ClientId
 $appPrincipal = Register-SPAppPrincipal -DisplayName $appName -NameIdentifier $appIdentifier -Site $web 
@@ -30,10 +29,11 @@ Write-Host "App published"
 exit
 
 # List App Principals
+$clientId = "bdc2fb21-acc2-418b-b061-2430afc4207e"
 $Url = "http://sp2016"
 $web = Get-SPWeb -Identity $Url
 $realm = Get-SPAuthenticationRealm -ServiceContext $web.Site;
-$appIdentifier = $issuerId  + '@' + $realm;
+$appIdentifier = $clientId + '@' + $realm;
 Get-SPAppPrincipal -NameIdentifier $appIdentifier -Site $Url
 
 exit

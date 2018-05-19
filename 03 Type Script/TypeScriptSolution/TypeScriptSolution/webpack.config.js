25096ec2bb20f4a3f49011968d1c362ec6f4d0e3
Alexander Pajer <alexander.pajer@integrations.at>
2018-05-19 10:13:40 +0200

    ts update

--- 03 Type Script/TypeScriptSolution/TypeScriptSolution/webpack.config.js
+++ 03 Type Script/TypeScriptSolution/TypeScriptSolution/webpack.config.js
@@ -1,23 +1,22 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index',
-    mode: 'development', //'production'
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-            },
-        ],
-    },
-    resolve: {
-        extensions: [
-            '.ts',
-        ],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './Layouts/TypeScriptSolution/dist'),
-    }
+  entry: "./src/index",
+  mode: "development", //'production'
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts"]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./Layouts/TypeScriptSolution/dist")
+  }
 };
